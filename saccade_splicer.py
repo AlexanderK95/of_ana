61378b942ef3eca205ca9ca841069bd67bb6b7e6
Kressa <kressa@students.uni-marburg.de>
2022-09-26 12:44:30 +0200

    basic saccade detection

---
+++ saccade_splicer.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import cv2 as cv
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+def shiftToZero(a):
+    return ((a+180)%360)-180
+
+def derivative(arrX, arrY):
+    out = np.zeros(arrX.size)
+    for i in np.arange(1,arrX.size-1):
+        out[i] = (arrY[i+1]-arrY[i-1]) / (arrX[i+1]-arrX[i-1])
+    return out
+
+gaze_df = pd.read_csv("Data/20220923-131939_1_gaze.csv", sep=",")
+speed = np.sqrt(gaze_df['rot.x'] ** 2 + gaze_df['rot.z'] ** 2)
+# plt.plot(gaze_df['time'], speed)
+# plt.plot(gaze_df['time'], gaze_df['rot.y'])
+# plt.plot(gaze_df['time'], gaze_df['rot.z'])
+# plt.plot(gaze_df['time'], gaze_df['rot.x'].map(shiftToZero))
+# plt.plot(gaze_df['time'], gaze_df['rot.y'].map(shiftToZero))
+# plt.plot(gaze_df['time'], gaze_df['rot.z'].map(shiftToZero))
+# plt.show()
+
+x = gaze_df['time'][0:500].map(shiftToZero)
+y1 = gaze_df['rot.x'][0:500].map(shiftToZero)
+y2 = gaze_df['rot.y'][0:500].map(shiftToZero)
+dy1 = derivative(x,y1)    #velocity
+dy2 = derivative(x,y2)    #velocity
+speed = np.sqrt(dy1**2 + dy2**2)
+# ddy = derivative(x,dy)  #acceleration
+# dddy = derivative(x,ddy) #jerk
+plt.plot(x,y1)
+plt.plot(x,y2)
+plt.plot(x,speed/30)
+# plt.plot(x,dy/10)
+# plt.plot(x,ddy/1000)
+# plt.plot(x,dddy/10000)
+plt.show()
+
+# print((gaze_df['time'].iloc[-1]-gaze_df['time'].iloc[0])/gaze_df['time'].size)
