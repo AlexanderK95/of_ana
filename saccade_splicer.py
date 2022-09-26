f7b359f5f1a2393276dd513dbb4dad55101697c0
Kressa <kressa@students.uni-marburg.de>
2022-09-26 15:48:38 +0200

    acceleration

--- saccade_splicer.py
+++ saccade_splicer.py
@@ -13,7 +13,7 @@ def derivative(arrX, arrY):
         out[i] = (arrY[i+1]-arrY[i-1]) / (arrX[i+1]-arrX[i-1])
     return out
 
-gaze_df = pd.read_csv("Data/20220923-131939_1_gaze.csv", sep=",")
+gaze_df = pd.read_csv("Data/20220926-142032_2_gaze_hr.csv", sep=",")
 speed = np.sqrt(gaze_df['rot.x'] ** 2 + gaze_df['rot.z'] ** 2)
 # plt.plot(gaze_df['time'], speed)
 # plt.plot(gaze_df['time'], gaze_df['rot.y'])
@@ -23,17 +23,19 @@ speed = np.sqrt(gaze_df['rot.x'] ** 2 + gaze_df['rot.z'] ** 2)
 # plt.plot(gaze_df['time'], gaze_df['rot.z'].map(shiftToZero))
 # plt.show()
 
-x = gaze_df['time'][0:500].map(shiftToZero)
-y1 = gaze_df['rot.x'][0:500].map(shiftToZero)
-y2 = gaze_df['rot.y'][0:500].map(shiftToZero)
+x = gaze_df['time'][0:500]
+y1 = gaze_df['rot.x'][0:500]
+y2 = gaze_df['rot.y'][0:500]
 dy1 = derivative(x,y1)    #velocity
 dy2 = derivative(x,y2)    #velocity
 speed = np.sqrt(dy1**2 + dy2**2)
+accel = derivative(x, speed)
 # ddy = derivative(x,dy)  #acceleration
 # dddy = derivative(x,ddy) #jerk
 plt.plot(x,y1)
 plt.plot(x,y2)
 plt.plot(x,speed/30)
+plt.plot(x,accel/3000)
 # plt.plot(x,dy/10)
 # plt.plot(x,ddy/1000)
 # plt.plot(x,dddy/10000)
